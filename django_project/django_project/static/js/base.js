48f250747a82b43a307dd2d19ae7b393fe182e95
Alberto <albertferrico@gmail.com>
2017-11-07 03:32:26 -0500

    ultimas actualizaciones

--- django_project/django_project/static/js/base.js
+++ django_project/django_project/static/js/base.js
@@ -1,3 +1,24 @@
-document.querySelector(".site-menu-container").addEventListener("click",function(){
-	document.querySelector(".site-menu-container ul").classList.toggle("show")
-})
+//document.querySelector(".site-menu-container").addEventListener("click",function(){
+//	document.querySelector(".site-menu-container ul").classList.toggle("show")
+//})
+$(document).ready(function(){
+	$('site-menu-container').on('click', function(){
+		$('.site-menu-container ul').toggle('show');
+	});
+	var amountScrolled = 300;
+
+	$(window).scroll(function() {
+    		if ( $(window).scrollTop() > amountScrolled ) {
+        		$('a.scrollToTop').fadeIn('slow');
+    		} else {
+        		$('a.scrollToTop').fadeOut('slow');
+    		}
+	});
+	
+	$('a.scrollToTop').click(function() {
+    		$('html, body').animate({
+        		scrollTop: 0
+    		}, 700);
+    		return false;
+	});
+});
